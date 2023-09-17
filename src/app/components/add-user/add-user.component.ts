0fde123cb21a1116d27188614f12447702fdf89d
oleksii <oleksii.iepikhin@gmail.com>
2023-09-17 22:59:30 +0200

    Fetch user data using interface.

--- src/app/components/add-user/add-user.component.ts
+++ src/app/components/add-user/add-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/interfaces/user';
 import { ApiService } from 'src/app/services/api.service';
 
 @Component({
@@ -7,13 +8,27 @@ import { ApiService } from 'src/app/services/api.service';
   styleUrls: ['./add-user.component.scss']
 })
 export class AddUserComponent implements OnInit {
+  users: User[] = [];
 
   constructor(
     private apiService: ApiService
   ) {}
   
   ngOnInit(): void {
-    this.apiService.getUserData(10).subscribe((user: any) => console.log('user: ', user));
+    this.apiService.getUserData(10).subscribe(
+      (data: any) => {
+        data.results.forEach((user: any) => {
+          this.users.push({
+            name: user.name.first + ' ' + user.name.last,
+            gender: user.gender,
+            profileImage: user.picture.thumbnail,
+            location: user.location.country + ', ' + user.location.city + ', ' + user.location.street.name + ', ' + user.location.street.number,
+            email: user.email
+          });
+        });
+      }
+    );
+
     this.apiService.getWeatherData('-45.7179', '-84.3750').subscribe((weather: any) => console.log('weather: ', weather));
   }
 }
