2d2f1cc96283ecf550dc5f2e90483a45e310632a
oleksii <oleksii.iepikhin@gmail.com>
2023-09-17 22:11:22 +0200

    Add components to add / show users.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddUserComponent } from './add-user/add-user.component';
+import { ShowUserComponent } from './show-user/show-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AddUserComponent,
+    title: 'Add User'
+  },
+  {
+    path: 'show',
+    component: ShowUserComponent,
+    title: 'Show User'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/show-user/show-user.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-show-user',
+  templateUrl: './show-user.component.html',
+  styleUrls: ['./show-user.component.scss']
+})
+export class ShowUserComponent {}
