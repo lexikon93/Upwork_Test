8f7673e1e16f266c152706062a235281abdc1312
oleksii <oleksii.iepikhin@gmail.com>
2023-09-24 23:05:59 +0200

    Add map component to show user location / profile image.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddUserComponent } from './components/add-user/add-user.component';
 import { ShowUserComponent } from './components/show-user/show-user.component';
+import { MapComponent } from './components/map/map.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AddUserComponent,
-    ShowUserComponent
+    ShowUserComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
