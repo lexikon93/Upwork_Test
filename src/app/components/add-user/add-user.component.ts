760eadf8beabdc2bd06c9c9763bca5ae223a239a
oleksii <oleksii.iepikhin@gmail.com>
2023-09-24 23:53:15 +0200

    Update current temperature periodically (every 5 minutes).

--- src/app/components/add-user/add-user.component.ts
+++ src/app/components/add-user/add-user.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, map, Observable, Subscription } from 'rxjs';
 import { User } from 'src/app/interfaces/user';
 import { ApiService } from 'src/app/services/api.service';
 
@@ -7,99 +8,132 @@ import { ApiService } from 'src/app/services/api.service';
   templateUrl: './add-user.component.html',
   styleUrls: ['./add-user.component.scss']
 })
-export class AddUserComponent implements OnInit {
+export class AddUserComponent implements OnInit, OnDestroy {
   users: User[] = [];
 
   savedUsers: User[] = [];
+
+  refreshSubscription: Subscription | undefined;
 
   constructor(
     private apiService: ApiService
   ) {}
   
   ngOnInit(): void {
-    this.apiService.getUserData(10).subscribe(
-      (data: any) => {
+    this.loadData().subscribe(() => {
+      this.updateWeatherData();
+
+      this.refreshSubscription = interval(5 * 60 * 1000).subscribe(() => {
+        this.updateWeatherData();
+      });
+    });
+  }
+
+  loadData(): Observable<void> {
+    return this.apiService.getUserData(10).pipe(
+      map((data: any) => {
         data.results.forEach((user: any) => {
           this.users.push({
-            name: user.name.first + ' ' + user.name.last,
+            name: user.name.first + ' ' + user.name.last, 
             gender: user.gender,
             profileImage: user.picture.medium,
-            location: user.location.country + ', ' + user.location.city + ', ' + user.location.street.name + ', ' + user.location.street.number,
+            location:
+              user.location.country + ', ' +
+              user.location.city + ', ' +
+              user.location.street.name + ', ' +
+              user.location.street.number,
             email: user.email,
             latitude: user.location.coordinates.latitude,
             longitude: user.location.coordinates.longitude,
             icon: '',
-            temperature: '',
+            temperature: ''
           });
         });
-
-        this.users.forEach(user => {
-          this.apiService.getWeatherData(user.latitude, user.longitude).subscribe(
-            (weather: any) => {
-              let icon = '';
-              switch (weather.current_weather.weathercode.toString()) {
-                case '1':
-                case '2':
-                case '3':
-                  icon = 'Mainly clear, partly cloudy, and overcast';
-                  break;
-                case '45':
-                case '48':
-                  icon = 'Fog and depositing rime fog';
-                  break;
-                case '51':
-                case '53':
-                case '55':
-                  icon = 'Drizzle: Light, moderate, and dense intensity';
-                  break;
-                case '56':
-                case '57':
-                  icon = 'Freezing Drizzle: Light and dense intensity';
-                  break;
-                case '61':
-                case '63':
-                case '65':
-                  icon = 'Rain: Slight, moderate and heavy intensity';
-                  break;
-                case '66':
-                case '67':
-                  icon = 'Freezing Rain: Light and heavy intensity';
-                  break;
-                case '71':
-                case '73':
-                case '75':
-                  icon = 'Snow fall: Slight, moderate, and heavy intensity';
-                  break;
-                case '77':
-                  icon = 'Snow grains';
-                  break;
-                case '80':
-                case '81':
-                case '82':
-                  icon = 'Rain showers: Slight, moderate, and violent';
-                  break;
-                case '85':
-                case '86':
-                  icon = 'Snow showers slight and heavy';
-                  break;
-                case '95':
-                  icon = 'Thunderstorm: Slight or moderate';
-                  break;
-                case '96':
-                case '99':
-                  icon = 'Thunderstorm with slight and heavy hail';
-                  break;
-                default:
-                  icon = 'Clear sky';
-              }
-
-              user.icon = icon;
-              user.temperature = 'Current: ' + weather.current_weather.temperature + ', Lowest: ' + Math.min(...weather.hourly.temperature_2m) + ', Highest: ' + Math.max(...weather.hourly.temperature_2m);
-            } 
-          );
-        });
-      }
+      })
     );
+  }
+
+  updateWeatherData(): void {
+    this.users.forEach(user => {
+      this.apiService.getWeatherData(user.latitude, user.longitude).subscribe(
+        (weather: any) => {
+          user.icon = this.getWeatherIcon(weather.current_weather.weathercode);
+          user.temperature = this.getTemperatureString(weather.current_weather.temperature, weather.hourly.temperature_2m);
+        }
+      );
+    });
+  }
+
+  getWeatherIcon(weatherCode: string): string {
+    let icon = '';
+
+    switch (weatherCode.toString()) {
+      case '1':
+          case '2':
+          case '3':
+            icon = 'Mainly clear, partly cloudy, and overcast';
+            break;
+          case '45':
+          case '48':
+            icon = 'Fog and depositing rime fog';
+            break;
+          case '51':
+          case '53':
+          case '55':
+            icon = 'Drizzle: Light, moderate, and dense intensity';
+            break;
+          case '56':
+          case '57':
+            icon = 'Freezing Drizzle: Light and dense intensity';
+            break;
+          case '61':
+          case '63':
+          case '65':
+            icon = 'Rain: Slight, moderate and heavy intensity';
+            break;
+          case '66':
+          case '67':
+            icon = 'Freezing Rain: Light and heavy intensity';
+            break;
+          case '71':
+          case '73':
+          case '75':
+            icon = 'Snow fall: Slight, moderate, and heavy intensity';
+            break;
+          case '77':
+            icon = 'Snow grains';
+            break;
+          case '80':
+          case '81':
+          case '82':
+            icon = 'Rain showers: Slight, moderate, and violent';
+            break;
+          case '85':
+          case '86':
+            icon = 'Snow showers slight and heavy';
+            break;
+          case '95':
+            icon = 'Thunderstorm: Slight or moderate';
+            break;
+          case '96':
+          case '99':
+            icon = 'Thunderstorm with slight and heavy hail';
+            break;
+          default:
+            icon = 'Clear sky';
+    }
+    
+    return icon;
+  }
+
+  getTemperatureString(current: number, temperatures: number[]): string {
+    return 'Current: ' + current + ', Lowest: ' + Math.min(...temperatures) + ', Highest: ' + Math.max(...temperatures);
+  }
+
+  ngOnDestroy(): void {
+    if (this.refreshSubscription) {
+      this.refreshSubscription.unsubscribe();
+    }
   }
 
   saveUser(user: User): void {
